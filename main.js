54c22ea0f6e27e2a9f58af7df4a6056394977bcb
bigbear <xiongzhengyu@hqyxjy.com>
2017-07-28 20:13:49 +0800

    增加输出功能，增加取消task选择功能，解决先选coder再选task引发的bug，解决task和coder的选中状态会被鼠标晃动导致取消的bug

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var isOnSelect = false;
 var leftTaskList = [];
 var coderList = [];
-var currentChooseTask;
-var currentChooseCoder;
+var currentChooseTask = null;
+var currentChooseCoder = null;
 
 //-------------------------------------------------------------------------------------------//
 
@@ -12,6 +12,8 @@ function inputTask() {
 }
 
 function initTasksData(tasksText) {
+    if (tasksText === null || tasksText === "") return;
+
     var taskStringList = tasksText.split("\n");
     // 迷之不可以，代码提示完全不好用
     // for (var taskString in taskStringList) {
@@ -25,46 +27,58 @@ function initTasksData(tasksText) {
 }
 
 function renderTaskGroup(taskGroup) {
+    if (taskGroup.length === 0) return;
+
     var container = document.getElementById('task');
     // container.remove
     for (var i = 0; i < taskGroup.length; i++) {
         var child = document.createElement('div');
 
         var ownerName = "";
-        if(taskGroup[i].owner!==null){
+        if (taskGroup[i].owner !== null) {
             ownerName = taskGroup[i].owner.name;
         }
         child.innerHTML = taskGroup[i].name + " : " + taskGroup[i].hour + "h -> " + ownerName;
-        child.style.padding = 15;
+        child.style.padding = 10;
 
         child.onmouseover = function () {
             // 用this而不是child，否则会绑定成最后一个
-            this.style.backgroundColor = 'green'
+            if (this.style.backgroundColor === "") {
+                this.style.backgroundColor = 'green'
+            }
         };
         child.onmouseout = function () {
             if (this.style.backgroundColor === 'green') {
-                this.style.backgroundColor = 'transparent'
+                this.style.backgroundColor = ""
             }
         };
 
-        child.onclick = (function (inner_i,inner_child) {
+        child.onclick = (function (inner_task, inner_child) {
             return function () {
                 if (isOnSelect) {
                     if (currentChooseCoder !== null) {
-                        if (contains(leftTaskList, taskGroup[inner_i])) {
-                            removeByObj(leftTaskList, taskGroup[inner_i]);
+                        if (contains(leftTaskList, inner_task)) {
+                            removeByObj(leftTaskList, inner_task);
                         }
-                        currentChooseCoder.bind(taskGroup[inner_i]);
+                        currentChooseCoder.bind(inner_task);
+                    } else {
+                        // 点的是这个，而且这个之前被绑定了
+                        if (currentChooseTask === inner_task && currentChooseTask.owner !== null) {
+                            currentChooseTask.unbind();
+                            if (!contains(leftTaskList, currentChooseTask)) {
+                                leftTaskList.push(currentChooseTask);
+                            }
+                        }
                     }
                     exitSelectMode();
                     update();
                 } else {
                     isOnSelect = true;
-                    currentChooseTask = taskGroup[inner_i];
+                    currentChooseTask = inner_task;
                     inner_child.style.backgroundColor = 'red';
                 }
             }
-        }(i,child));
+        }(taskGroup[i], child));
 
         container.appendChild(child);
     }
@@ -78,6 +92,8 @@ function inputCoder() {
 }
 
 function initCodersData(codersText) {
+    if (codersText === null || codersText === "") return;
+
     var coderStringList = codersText.split("\n");
     for (var i = 0; i < coderStringList.length; i++) {
         coderList.push(new Coder(coderStringList[i]));
@@ -85,40 +101,44 @@ function initCodersData(codersText) {
 }
 
 function renderCoders() {
+    if (coderList.length === 0) return;
+
     var container = document.getElementById('coder');
     for (var i = 0; i < coderList.length; i++) {
         var child = document.createElement('div');
         child.innerHTML = coderList[i].name + " : " + coderList[i].getAllTime() + "h";
-        child.style.padding = 15;
+        child.style.padding = 10;
 
         child.onmouseover = function () {
             // 用this而不是child，否则会绑定成最后一个
-            this.style.backgroundColor = 'blue'
+            if (this.style.backgroundColor === "") {
+                this.style.backgroundColor = 'blue'
+            }
         };
         child.onmouseout = function () {
             if (this.style.backgroundColor === 'blue') {
-                this.style.backgroundColor = 'transparent'
+                this.style.backgroundColor = ""
             }
         };
 
-        child.onclick = (function (inner_i,inner_child) {
+        child.onclick = (function (inner_coder, inner_child) {
             return function () {
                 if (isOnSelect) {
                     if (currentChooseTask !== null) {
                         if (contains(leftTaskList, currentChooseTask)) {
                             removeByObj(leftTaskList, currentChooseTask);
                         }
-                        currentChooseTask.bind(coderList[inner_i]);
+                        currentChooseTask.bind(inner_coder);
                     }
                     exitSelectMode();
                     update();
                 } else {
                     isOnSelect = true;
-                    currentChooseCoder = coderList[inner_i];
+                    currentChooseCoder = inner_coder;
                     inner_child.style.backgroundColor = 'red';
                 }
             }
-        }(i,child));
+        }(coderList[i], child));
 
         container.appendChild(child);
     }
@@ -164,11 +184,45 @@ function renderTasks() {
 //-------------------------------------------------------------------------------------------//
 
 function output() {
-    ttt = prompt("enter");
+    copyToClipboard(getDispatchTaskResult());
+}
 
-    if (ttt == "aaa") {
-        document.getElementById("console").innerHTML = "输入正确！";
+function getDispatchTaskResult() {
+    return getDispatchedTaskInfo() + getUnDispatchedTaskInfo();
+}
+
+function getDispatchedTaskInfo() {
+    var result = "任务分配情况<br /><br />";
+    for (var i = 0; i < coderList.length; i++) {
+        result += getOneCoderTaskInfo(coderList[i]);
     }
+    return result;
+}
+
+function getOneCoderTaskInfo(coder) {
+    if (coder.ownTaskList.length !== 0) {
+        var result = coder.name + " (" + coder.getAllTime() + "h)<br />";
+        for (var i = 0; i < coder.ownTaskList.length; i++) {
+            result += coder.ownTaskList[i].name + " - " + coder.ownTaskList[i].hour + "h<br />";
+        }
+        return result + "<br />";
+    } else {
+        return "";
+    }
+}
+
+function getUnDispatchedTaskInfo() {
+    if (leftTaskList.length !== 0) {
+        var coder = new Coder("<br /><br />未分配任务 ");
+        coder.ownTaskList = leftTaskList;
+        return getOneCoderTaskInfo(coder);
+    } else {
+        return "";
+    }
+}
+
+function copyToClipboard(txt) {
+    document.getElementById("console").innerHTML = txt;
 }
 
 //-------------------------------------------------------------------------------------------//
@@ -229,7 +283,7 @@ function readFile(filename) {
     var f = fso.OpenTextFile(filename, 1);
     var s = "";
     while (!f.AtEndOfStream)
-        s += f.ReadLine() + "\n";
+        s += f.ReadLine() + "<br />";
     f.Close();
     return s;
 }
